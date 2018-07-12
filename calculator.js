cd4739210ae4d9f002b0f7e8de707172c3bab002
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-07-11 21:28:50 -0300

    First version of CalculatorJS

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function insert(num){
+    document.form.textview.value = document.form.textview.value + num
+}
+function equal(){
+    var exp = document.form.textview.value
+    if(exp)
+        document.form.textview.value = eval(exp)
+}
+function clean(){
+    document.form.textview.value = " "
+}
+function goBack(){
+    var exp = document.form.textview.value
+    document.form.textview.value = exp.substring(0,exp.lenght-1)
+}
